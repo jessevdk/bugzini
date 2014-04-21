7df7151e57cc571ef89015f63a1d616169b6f00f
Jesse van den Kieboom <jessevdk@gnome.org>
2014-04-21 20:48:28 +0200

    Implement after query for comments

--- bug.go
+++ bug.go
@@ -7,6 +7,7 @@ import (
 	"bugzilla"
 	"os"
 	"encoding/gob"
+	"time"
 )
 
 type BugsCache struct {
@@ -97,9 +98,14 @@ func BugCommentsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if len(bug.Comments) != 0 {
-		JsonResponse(w, bug.Comments)
-		return
+	after := r.FormValue("after")
+
+	// Only use cache if not asking for bugs after a certain date
+	if len(after) == 0 {
+		if len(bug.Comments) != 0 {
+			JsonResponse(w, bug.Comments)
+			return
+		}
 	}
 
 	client, err := Bz()
@@ -109,16 +115,40 @@ func BugCommentsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	comments, err := client.Bugs().GetComments(client, id)
+	var comments []bugzilla.Comment
+
+	if len(after) == 0 {
+		comments, err = client.Bugs().GetComments(client, id)
+	} else {
+		afsec, err := strconv.ParseInt(after, 10, 64)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		t := time.Unix(afsec, 0)
+		comments, err = client.Bugs().GetCommentsAfter(client, id, t)
+	}
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	bug.Comments = comments
-	bugsCache.Save()
+	if len(after) == 0 {
+		bug.Comments = comments
+	} else if len(bug.Comments) > 0 {
+		last := bug.Comments[len(bug.Comments) - 1]
 
+		for _, v := range comments {
+			if v.Time.After(last.Time) {
+				bug.Comments = append(bug.Comments, v)
+			}
+		}
+	}
+
+	bugsCache.Save()
 	JsonResponse(w, comments)
 }
 
