8dfcd6ee26101b34a43b77aee0bda75fe942d6e8
Jesse van den Kieboom <jessevdk@gnome.org>
2014-04-22 00:50:48 +0200

    Cache xmlrpc connection

--- bz.go
+++ bz.go
@@ -4,9 +4,19 @@ import (
 	"bugzilla"
 )
 
+var bz *bugzilla.Conn
+
 func Bz() (*bugzilla.Conn, error) {
-	return bugzilla.Dial(bugzilla.Address{
+	if bz != nil {
+		return bz, nil
+	}
+
+	var err error
+
+	bz, err = bugzilla.Dial(bugzilla.Address{
 		Host:   options.Bugzilla.Host,
 		Secure: options.Bugzilla.Secure,
 	})
+
+	return bz, err
 }
